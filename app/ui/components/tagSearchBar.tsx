7da671d158a028a3a22e267c7257dae4c5705df1
Sebastian <152189730+5ebastian-E@users.noreply.github.com>
2024-09-11 15:32:33 +0200

    Update tagSearchBar.tsx

--- app/ui/components/tagSearchBar.tsx
+++ app/ui/components/tagSearchBar.tsx
@@ -2,13 +2,12 @@ import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
 
 interface AutocompleteProps {
   availableTags: string[];
-  onSelectTags: (tags: string[]) => void; // Add this prop for tag selection
+  onSelectTags: (tags: string[]) => void;
 }
 
 const Autocomplete: React.FC<AutocompleteProps> = ({ availableTags, onSelectTags }) => {
   const [inputValue, setInputValue] = useState<string>('');
   const [filteredTags, setFilteredTags] = useState<string[]>([]);
-  const [selectedTag, setSelectedTag] = useState<string | null>(null);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -26,17 +25,12 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ availableTags, onSelectTags
 
   const handleInputKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
+      event.preventDefault();
       const tags = inputValue.split(',').map(tag => tag.trim()).filter(tag => tag.length > 0);
       onSelectTags(tags);
       setInputValue('');
       setFilteredTags([]);
     }
-  };
-
-  const handleTagClick = (tag: string) => {
-    setSelectedTag(tag);
-    setInputValue(tag);
-    setFilteredTags([]);
   };
 
   return (
@@ -55,19 +49,21 @@ const Autocomplete: React.FC<AutocompleteProps> = ({ availableTags, onSelectTags
           {filteredTags.map((tag, index) => (
             <li
               key={index}
-              onClick={() => handleTagClick(tag)}
+              onClick={() => {
+                const tags = inputValue.split(',').map(tag => tag.trim()).filter(tag => tag.length > 0);
+                if (!tags.includes(tag)) {
+                  tags.push(tag);
+                }
+                onSelectTags(tags);
+                setInputValue('');
+                setFilteredTags([]);
+              }}
               className={`text-black px-3 py-2 cursor-pointer ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-200`}
             >
               {tag}
             </li>
           ))}
         </ul>
-      )}
-
-      {selectedTag && (
-        <div className="mt-2 text-black">
-          <strong>Selected Tag: </strong>{selectedTag}
-        </div>
       )}
     </div>
   );
