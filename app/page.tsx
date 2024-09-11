ebebf153284d3b115cafb18db08590605da78ccd
Sebastian <152189730+5ebastian-E@users.noreply.github.com>
2024-09-11 16:31:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,381 +1,10 @@
-"use client"
-import { useState } from 'react';
+"use client";
+import { useState, ChangeEvent, KeyboardEvent } from 'react';
 import Image from 'next/image';
-import Autocomplete from './ui/components/tagSearchBar'
 
 export default function Home() {
   const gameData = [
-    {
-      id: 1,
-      name: 'The Legend of Zelda: Breath of the Wild',
-      image: './zeldabotw.jpg',
-      rating: 8.4,
-      tags: ['open world', 'action-adventure', 'exploration'],
-    },
-    {
-      id: 2,
-      name: 'God of War',
-      image: './gow2014.jpg',
-      rating: 9.2,
-      tags: ['story-driven', 'action', 'hack-and-slash', 'mythology'],
-    },
-    {
-      id: 3,
-      name: 'Red Dead Redemption 2',
-      image: './reddead2.png',
-      rating: 8.2,
-      tags: ['open world', 'story-driven', 'western', 'action'],
-    },
-    {
-      id: 4,
-      name: 'Minecraft',
-      image: './minecraft.jpg',
-      rating: 7.5,
-      tags: ['sandbox', 'survival', 'creative', 'multiplayer'],
-    },
-    {
-      id: 5,
-      name: 'Subnautica',
-      image: './subnautica.jpg',
-      rating: 8.1,
-      tags: ['open world', 'survival', 'exploration', 'underwater'],
-    },
-    {
-      id: 6,
-      name: 'Valorant',
-      image: './valorant.jpg',
-      rating: 5.8,
-      tags: ['multiplayer', 'shooter', 'FPS', 'competitive'],
-    },
-    {
-      id: 7,
-      name: 'Elden Ring',
-      image: './eldenring.jpg',
-      rating: 9.5,
-      tags: ['open world', 'souls-like', 'RPG', 'fantasy'],
-    },
-    {
-      id: 8,
-      name: 'CS:GO',
-      image: './csgo.jpg',
-      rating: 7.2,
-      tags: ['shooter', 'multiplayer', 'FPS', 'competitive'],
-    },
-    {
-      id: 9,
-      name: 'Cyberpunk 2077',
-      image: './cyberpunk.jpg',
-      rating: 7.6,
-      tags: ['open world', 'RPG', 'futuristic', 'story-driven'],
-    },
-    {
-      id: 10,
-      name: 'Inscryption',
-      image: './inscryption.jpg',
-      rating: 9.0,
-      tags: ['card game', 'horror', 'puzzle', 'roguelike'],
-    },
-    {
-      id: 11,
-      name: 'The Witcher 3',
-      image: './witcher3.jpg',
-      rating: 7.9,
-      tags: ['open world', 'RPG', 'fantasy', 'story-driven'],
-    },
-    {
-      id: 12,
-      name: 'Dark Souls 3',
-      image: './ds3.jpg',
-      rating: 6.5,
-      tags: ['souls-like', 'RPG', 'action', 'fantasy'],
-    },
-    {
-      id: 13,
-      name: 'For Honor',
-      image: './forhonor.jpg',
-      rating: 6.4,
-      tags: ['multiplayer', 'action', 'melee combat', 'historical'],
-    },
-    {
-      id: 14,
-      name: 'Hollow Knight',
-      image: './hollowknight.jpg',
-      rating: 7.5,
-      tags: ['metroidvania', 'platformer', 'action', 'indie'],
-    },
-    {
-      id: 15,
-      name: 'Path of Exile',
-      image: './poe.jpg',
-      rating: 6.8,
-      tags: ['RPG', 'action', 'hack-and-slash', 'multiplayer'],
-    },
-    {
-      id: 16,
-      name: 'Dont Starve Together',
-      image: './dst.jpg',
-      rating: 6.5,
-      tags: ['survival', 'multiplayer', 'indie', 'crafting'],
-    },
-    {
-      id: 17,
-      name: 'Warhammer Total War 3',
-      image: './whtw3.jpg',
-      rating: 8.6,
-      tags: ['strategy', 'RPG', 'fantasy', 'tactical'],
-    },
-    {
-      id: 18,
-      name: 'League of Legends',
-      image: './lol.jpg',
-      rating: 7.0,
-      tags: ['MOBA', 'multiplayer', 'competitive', 'strategy'],
-    },
-    {
-      id: 19,
-      name: 'Once Human',
-      image: './oncehuman.jpg',
-      rating: 6.2,
-      tags: ['survival', 'RPG', 'horror', 'multiplayer'],
-    },
-    {
-      id: 20,
-      name: 'Warframe',
-      image: './warframe.png',
-      rating: 7.3,
-      tags: ['RPG', 'action', 'multiplayer', 'sci-fi'],
-    },
-    {
-      id: 21,
-      name: 'Portal',
-      image: './portal.jpg',
-      rating: 8.1,
-      tags: ['puzzle', 'sci-fi', 'first-person'],
-    },
-    {
-      id: 22,
-      name: 'Portal 2',
-      image: './portal2.jpg',
-      rating: 8.2,
-      tags: ['puzzle', 'co-op', 'sci-fi', 'first-person'],
-    },
-    {
-      id: 23,
-      name: 'Age of Mythology',
-      image: './aom.jpg',
-      rating: 8.9,
-      tags: ['strategy', 'mythology', 'RTS'],
-    },
-    {
-      id: 24,
-      name: 'Chained Together',
-      image: './chainedtogether.jpg',
-      rating: 5,
-      tags: ['puzzle', 'indie', 'co-op'],
-    },
-    {
-      id: 25,
-      name: 'Baldurs Gate 3',
-      image: './bg3.jpg',
-      rating: 9.4,
-      tags: ['RPG', 'fantasy', 'story-driven', 'co-op'],
-    },
-    {
-      id: 26,
-      name: 'Apex Legends',
-      image: './apex.jpg',
-      rating: 5,
-      tags: ['battle royale', 'shooter', 'multiplayer', 'FPS'],
-    },
-    {
-      id: 27,
-      name: 'Brawlhalla',
-      image: './brawlhalla.jpg',
-      rating: 4.9,
-      tags: ['fighting', 'multiplayer', 'platformer'],
-    },
-    {
-      id: 28,
-      name: 'Dark and Darker',
-      image: './dnd.jpg',
-      rating: 6.4,
-      tags: ['RPG', 'fantasy', 'dungeon crawler', 'multiplayer'],
-    },
-    {
-      id: 29,
-      name: 'Death must Die',
-      image: './dmd.jpg',
-      rating: 5.9,
-      tags: ['RPG', 'action', 'roguelike'],
-    },
-    {
-      id: 30,
-      name: 'Dishonored',
-      image: './dishonored.jpg',
-      rating: 7.8,
-      tags: ['stealth', 'action', 'RPG', 'story-driven'],
-    },
-    {
-      id: 31,
-      name: 'Green Hell',
-      image: './gh.jpg',
-      rating: 6.8,
-      tags: ['survival', 'open world', 'multiplayer'],
-    },
-    {
-      id: 32,
-      name: 'Lost in Random',
-      image: './lir.jpg',
-      rating: 8.9,
-      tags: ['adventure', 'action', 'indie', 'story-driven'],
-    },
-    {
-      id: 33,
-      name: 'SCP Secret Laboratory',
-      image: './scp.jpg',
-      rating: 8.1,
-      tags: ['horror', 'multiplayer', 'survival'],
-    },
-    {
-      id: 34,
-      name: 'GTA 5',
-      image: './gta.jpg',
-      rating: 7.1,
-      tags: ['open world', 'action', 'multiplayer', 'story-driven'],
-    },
-    {
-      id: 35,
-      name: 'Bioshock',
-      image: './bioshock.jpg',
-      rating: 8.5,
-      tags: ['shooter', 'sci-fi', 'story-driven', 'horror'],
-    },
-    {
-      id: 36,
-      name: 'Skyrim',
-      image: './skyrim.jpg',
-      rating: 6.1,
-      tags: ['open world', 'RPG', 'fantasy', 'story-driven'],
-    },
-    {
-      id: 37,
-      name: 'The Legend of Zelda: Twilight Princess',
-      image: './tloztp.jpg',
-      rating: 6.2,
-      tags: ['action-adventure', 'fantasy', 'story-driven'],
-    },
-    {
-      id: 38,
-      name: 'Overwatch',
-      image: './overwatch.jpg',
-      rating: 6.5,
-      tags: ['shooter', 'multiplayer', 'FPS', 'team-based'],
-    },
-    {
-      id: 39,
-      name: 'Civisisation 6',
-      image: './civ6.jpg',
-      rating: 5,
-      tags: ['strategy', 'turn-based', 'historical'],
-    },
-    {
-      id: 40,
-      name: 'Slay the Princess',
-      image: './stp.jpg',
-      rating: 7.2,
-      tags: ['visual novel', 'horror', 'story-driven'],
-    },
-    {
-      id: 41,
-      name: 'Class of 09',
-      image: './co09.jpg',
-      rating: 6,
-      tags: ['visual novel', 'comedy', 'indie'],
-    },
-    {
-      id: 42,
-      name: 'The Coffin of Andy and Leyley',
-      image: './tcoaal.jpg',
-      rating: 8.6,
-      tags: ['visual novel', 'horror', 'story-driven'],
-    },
-    {
-      id: 43,
-      name: 'Undertale',
-      image: './undertale.jpg',
-      rating: 7,
-      tags: ['RPG', 'indie', 'story-driven', 'retro'],
-    },
-    {
-      id: 44,
-      name: 'Satisfactory',
-      image: './satisfactory.jpg',
-      rating: 6,
-      tags: ['sandbox', 'simulation', 'building', 'multiplayer'],
-    },
-    {
-      id: 45,
-      name: 'Hades',
-      image: './hades.jpg',
-      rating: 8.5,
-      tags: ['roguelike', 'action', 'RPG', 'indie'],
-    },
-    {
-      id: 46,
-      name: 'Stardew Valley',
-      image: './sdv.jpg',
-      rating: 6.6,
-      tags: ['simulation', 'farming', 'indie', 'multiplayer'],
-    },
-    {
-      id: 47,
-      name: 'Fortnite',
-      image: './fortnite.png',
-      rating: 7.1,
-      tags: ['battle royale', 'multiplayer', 'shooter', 'building'],
-    },
-    {
-      id: 48,
-      name: 'Rocket League',
-      image: './rl.png',
-      rating: 6.3,
-      tags: ['sports', 'multiplayer', 'action', 'competitive'],
-    },
-    {
-      id: 49,
-      name: 'Only Up',
-      image: './onlyup.jpg',
-      rating: 4,
-      tags: ['platformer', 'indie', 'adventure'],
-    },
-    {
-      id: 50,
-      name: 'It takes two',
-      image: './ittakestwo.jpg',
-      rating: 7.6,
-      tags: ['co-op', 'platformer', 'adventure', 'puzzle'],
-    },
-    {
-      id: 51,
-      name: 'Grounded',
-      image: './grounded.jpg',
-      rating: 7.9,
-      tags: ['survival', 'multiplayer', 'open world'],
-    },
-    {
-      id: 52,
-      name: 'Ark',
-      image: './ark.jpg',
-      rating: 5.9,
-      tags: ['survival', 'open world', 'multiplayer', 'dinosaurs'],
-    },
-    {
-      id: 53,
-      name: 'Dying Light 2',
-      image: './dl2.jpg',
-      rating: 8.1,
-      tags: ['open world', 'action', 'survival', 'zombies'],
-    },
+    `game data ....`
   ];
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
@@ -486,3 +115,72 @@ export default function Home() {
     </main>
   );
 }
+
+interface AutocompleteProps {
+  availableTags: string[];
+  onSelectTags: (tags: string[]) => void;
+}
+
+const Autocomplete: React.FC<AutocompleteProps> = ({ availableTags, onSelectTags }) => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [filteredTags, setFilteredTags] = useState<string[]>([]);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    if (value.length > 0) {
+      const filtered = availableTags.filter(tag =>
+        tag.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredTags(filtered);
+    } else {
+      setFilteredTags([]);
+    }
+  };
+
+  const handleInputKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const tags = inputValue.split(',').map(tag => tag.trim()).filter(tag => tag.length > 0);
+      onSelectTags(tags);
+      setInputValue('');
+      setFilteredTags([]);
+    }
+  };
+
+  return (
+    <div className="relative w-80">
+      <input
+        type="text"
+        placeholder="Search tags..."
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleInputKeyDown}
+        className="text-black w-full px-3 py-2 border border-gray-300 rounded-lg"
+      />
+
+      {filteredTags.length > 0 && (
+        <ul className="absolute top-10 left-0 w-full bg-white border border-gray-300 rounded-lg z-10 max-h-52 overflow-y-auto list-none p-0 m-0">
+          {filteredTags.map((tag, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                const tags = inputValue.split(',').map(tag => tag.trim()).filter(tag => tag.length > 0);
+                if (!tags.includes(tag)) {
+                  tags.push(tag);
+                }
+                onSelectTags(tags);
+                setInputValue('');
+                setFilteredTags([]);
+              }}
+              className={`text-black px-3 py-2 cursor-pointer ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} hover:bg-gray-200`}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
